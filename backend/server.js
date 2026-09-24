4e91ec98bc178613733135e1bd7acd5211f25a4b
Venkat Sunkara <venkatsunkara9959@gmail.com>
2026-09-24 12:32:53 +0530

    Prepare backend for production deployment

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 require("dotenv").config(); // to process env variables
 const express = require("express");
 const app = express();
+const cors = require("cors");
 
 const mongoose = require("mongoose");
 const jobRoutes = require("./routes/JobRoutes")
@@ -8,6 +9,7 @@ const authRoutes = require("./routes/authRoutes")
 
 
 // Middleware
+app.use(cors());
 app.use(express.json());
 
 // API routes
@@ -19,13 +21,15 @@ app.get("/", (req, res) => {
     res.send("Job Tracker Backend is running");
 });
 
+const PORT = process.env.PORT || 5000;
+
 //mongodb connection
 mongoose.connect(process.env.MONGO_URI)
     .then(() => {
         console.log("MONGO DB connected!");
         // Start server
-        app.listen(5000, () => {
-            console.log("SERVER running on PORT 5000");
+        app.listen(PORT, () => {
+            console.log(`SERVER running on PORT ${PORT}`);
         });
     })
     .catch((error) => {
